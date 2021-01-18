fa0abdb34436b7b51f3aaac24aba06499dc520d2
Pedro Gutiérrez <pedro.gutierrez@form3.tech>
2021-01-18 18:33:13 +0100

    code style

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-NODE_NAME="$CLUSTER_SERVICE@$(hostname).$CLUSTER_SERVICE.$CLUSTER_NAMESPACE.svc.cluster.local" _build/default/rel/clusterl/bin/clusterl console
+export NODE_NAME="$CLUSTER_SERVICE@$(hostname).$CLUSTER_SERVICE.$CLUSTER_NAMESPACE.svc.cluster.local" 
+_build/default/rel/clusterl/bin/clusterl console
